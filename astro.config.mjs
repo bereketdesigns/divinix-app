72054c8b7c43010fed5349fbde1ecd2539d451ca
bereketdesigns <bereketdesigns@gmail.com>
2025-10-10 10:44:44 +0000

    fix(deploy): Correct Vercel adapter for serverless deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // This tells Astro to build for a server environment
+  output: "server",
+
+  // This adapter configures Astro to deploy as a Vercel Serverless Function
+  adapter: vercel()
+});
